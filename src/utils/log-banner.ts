44abd150d8c65f9c9e22e8f8387e7e987826233b
Pedro Maia <26176931+Rylams@users.noreply.github.com>
2023-09-15 20:08:03 -0300

    Anna and rebrand

--- src/utils/log-banner.ts
+++ src/utils/log-banner.ts
@@ -3,11 +3,9 @@ import {readPackageSync} from 'read-pkg';
 
 const logBanner = () => {
   console.log(makeLines({
-    user: 'codetheweb',
-    repository: 'muse',
+    user: 'pedrohsmaia',
+    repository: 'pandora',
     version: readPackageSync().version,
-    paypalUser: 'codetheweb',
-    githubSponsor: 'codetheweb',
     madeByPrefix: 'Made with 🎶 by ',
     buildDate: process.env.BUILD_DATE ? new Date(process.env.BUILD_DATE) : undefined,
     commit: process.env.COMMIT_HASH ?? 'unknown',
